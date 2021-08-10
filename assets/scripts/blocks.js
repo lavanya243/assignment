93026a9b88c961646f05db2138f06e32fa6e938f
Lavanya Narayanan <69483128+lavanya243@users.noreply.github.com>
2021-08-10 17:15:40 +0530

    Add files via upload

--- assets/scripts/blocks.js
+++ assets/scripts/blocks.js
@@ -15,9 +15,10 @@ let time = date.getHours() + ":" + date.getMinutes();
 Blockly.Blocks['bot_block'] = {
   init: function() {
     this.appendStatementInput("NAME")
-        .setCheck(null)
+        //.setCheck(null)
         .appendField("Bot");
         this.setColour(130);
+
  this.setTooltip("");
  this.setHelpUrl("");
   }
@@ -26,68 +27,99 @@ Blockly.Blocks['bot_block'] = {
 Blockly.Blocks['dropdown_block'] = {
   init: function() {
     this.appendStatementInput("dropdown")
-        .setCheck(null)
+        //.setCheck(null)
         .appendField("Ask me a question:")
         .appendField(new Blockly.FieldDropdown([["What is the date today?",today], 
+                                     
                                                 ["What is the time now?",time], 
                                                 ["How are you?","I am doing good. Thank you."], 
                                                 ["What is JavaScript?","It is a scripting language for the web"], 
                                                 ["What is your name?","Lavanya Narayanan"]]), "input");
-    this.setPreviousStatement(true);
+    this.setPreviousStatement(true,"NAME");
     this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
+    this.setTooltip("");
+    this.setHelpUrl("");
   }
 };
-Blockly.JavaScript['bot_block'] = function(block) {
-  var statements_name = Blockly.JavaScript.statementToCode(block, 'NAME');
-
-  var code = `
-	var botValue = "${statements_name}";
-  `;
-  return code;
-};
-
+ 
 Blockly.JavaScript['dropdown_block'] = function (block) {
+  let text_input;
+  let code;
+  let inputTextValue;
+  Blockly.JavaScript['bot_block'] = function ()
+  {
+    text_input = block.getFieldValue("input");
+    code = `
+        inputTextValue = "${text_input}";
+              `;
+    return code;
+  };
+};
 
-  var text_input = block.getFieldValue("input");
-
-  let code = `
-	var inputTextValue = "${text_input}";
-  `;
-  return code;
+ /*
+Blockly.JavaScript['bot_block'] = function () {
+ 
+  Blockly.JavaScript['dropdown_block'] = function (block)
+  {
+    var text_input = block.getFieldValue("input");
+    var code = `
+    var inputTextValue = "${text_input}";
+    `;
+              
+    return code;
+  };
 
 };
 
+    //Blockly.JavaScript['dropdown_block']();
+ 
+  /*  Blockly.JavaScript['dropdown_block'] = function (block) {
+
+        let text_input = block.getFieldValue("input");
+        let code = `
+                  var inputTextValue = "${text_input}";
+                  `;
+        return code;
+      };*/
 
 
-var workspace = Blockly.inject("blocklyDiv", {
-  media: "assets/media/",
-  toolbox: document.getElementById("toolbox"),
-});
-
-function redrawUi() {
-  if (typeof inputTextValue !== "undefined") {
-    $("#inputBox").text(inputTextValue);
-  } else {
-    $("#inputBox").text("");
-  }
-}
-
-function runcode() {
-  
-  code = Blockly.JavaScript.workspaceToCode(workspace);
-  var geval = eval;
-  try {
-    geval(code);
-  } catch (e) {
-    console.error(e);
-  }
-  redrawUi();
-}
-
-function reset() {
-  delete inputTextValue;
-  Blockly.mainWorkspace.clear()
-  redrawUi();
-}
+    
+    var workspace = Blockly.inject("blocklyDiv", {
+      media: "assets/media/",
+      toolbox: document.getElementById("toolbox"),
+    });
+    
+    function redrawUi() {
+      if (typeof inputTextValue !== "undefined") {
+        $("#inputBox").text(inputTextValue);
+      } else {
+        $("#inputBox").text("");
+      }
+    }
+    
+    function runcode() {
+      // Generate JavaScript code and run it.
+      var geval = eval;
+      try {
+        geval(Blockly.JavaScript.workspaceToCode(workspace));
+      } catch (e) {
+        console.error(e);
+      }
+      redrawUi();
+/*
+      Blockly.JavaScript.addReservedWords('code');
+      var code = Blockly.JavaScript.workspaceToCode(workspace);
+      
+      try {
+        eval(code);
+      } catch (e) {
+        alert(e);
+      }
+      redrawUi();*/
+    }
+    
+    function reset() {
+      delete inputTextValue;
+      Blockly.mainWorkspace.clear()
+      redrawUi();
+    }
